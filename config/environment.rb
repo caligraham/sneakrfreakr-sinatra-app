be4fdd80feb4c90a248d243d705ed0b472791d37
Rebecca <graham.cali@gmail.com>
2021-01-07 14:13:19 -0600

    first commit

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,8 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+#require 'dotenv/load'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
 
 require './app/controllers/application_controller'
 require_all 'app'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20201226224925) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "image_url"
@@ -23,9 +26,11 @@ ActiveRecord::Schema.define(version: 20201226224925) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "image_url"
-    t.string "password_digest"
+    t.string   "username"
+    t.string   "image_url"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
