5da5a702b36210c68539ae3203c3da1733abc160
Rebecca <graham.cali@gmail.com>
2021-01-08 16:03:42 -0600

    updated route in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     #authorization helper for editing & deleting posts
